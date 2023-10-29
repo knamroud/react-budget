f30bbcf7b27cd8609d10ddfdfd1bce410d87c140
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-10-29 15:28:59 +0100

    Handle currency change in other components

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -4,6 +4,7 @@ import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
     const { dispatch,remaining  } = useContext(AppContext);
+    const { currency } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -59,16 +60,16 @@ const AllocationForm = (props) => {
                         <option defaultValue value="Add" name="Add">Add</option>
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
-
+                  <span style={{ marginLeft: '2rem' , size: 10}}>
+                    <label htmlFor="cost">{currency}</label>
                     <input
                         required='required'
                         type='number'
                         id='cost'
                         value={cost}
-                        style={{ marginLeft: '2rem' , size: 10}}
                         onChange={(event) => setCost(event.target.value)}>
                         </input>
-
+                    </span>
                     <button className="btn btn-primary" onClick={submitEvent} style={{ marginLeft: '2rem' }}>
                         Save
                     </button>
