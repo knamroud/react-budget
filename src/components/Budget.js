911d1435bc536e709a7ebf67fd39662585999c89
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-10-29 15:14:37 +0100

    Handle currency change

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,15 +3,32 @@ import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
     }
+    const handleCurrencyChange = (event) => {
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value
+        })
+    }
+
     return (
-<div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
-<input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
-</div>
+    <div className='alert alert-secondary'>
+    <span>Currency: {currency}</span>
+    <select className="custom-select" id="inputGroupSelect02" onChange={handleCurrencyChange}>
+        <option defaultValue value="£">£ Pound</option>
+        <option value="$">$ Dollar</option>
+        <option value="€">€ Euro</option>
+        <option value="₹">₹ Rupee</option>
+    </select>
+    <br></br>
+    <br></br>
+    <span>Budget: {currency}{budget}</span>
+    <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+    </div>
     );
 };
 export default Budget;
