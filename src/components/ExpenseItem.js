99819a4b4f8ef4f8e3edf9ea30635128f0f180ae
Kirelos Namroud <namroudkirelo04@gmail.com>
2023-10-29 15:37:03 +0100

    Update button and add - button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -26,11 +26,25 @@ const ExpenseItem = (props) => {
 
     }
 
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
+
     return (
         <tr>
         <td>{props.name}</td>
         <td>{currency}{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><button onClick={event => increaseAllocation(props.name)} style={{ borderRadius: '50%', backgroundColor: 'green', color: 'white', width: '30px', height: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '20px' }}>+</button></td>
+        <td><button onClick={event => decreaseAllocation(props.name)} style={{ borderRadius: '50%', backgroundColor: 'red', color: 'white', width: '30px', height: '30px', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '20px' }}>-</button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
